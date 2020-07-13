19dae4cb61f1f9ab4d06df9e85a63e558a64e3be
zachmonteith <zachmonteith@gmail.com>
2020-07-12 20:46:29 -0700

    refactored Knuth-Morris-Pratt

--- Search/kmpStringSearch.js
+++ Search/kmpStringSearch.js
@@ -1,7 +1,7 @@
 function kmpStringSearch(text, pattern){
   if (pattern.length === 0) return 0;
   let count = 0;
-  let helper = [];
+  let helper = [0];
   let i = 1;
   let j = 0;
   while (i < pattern.length){
@@ -28,4 +28,38 @@ function kmpStringSearch(text, pattern){
     } else i++;
   }
     return count;
+}
+
+function buildPatternArray(string){
+  var pattern = [0];
+  var prefidx = 0;
+  var sufidx = 1;
+  while (sufidx < string.length){
+    if (string[prefidx] === string[sufidx]){
+      pattern[sufidx] = prefidx+1;
+      prefidx++;
+      sufidx++;
+    } else if (prefidx === 0) {
+      pattern[sufidx] = 0;
+      sufidx++;
+    } else {
+      prefidx = pattern[prefidx-1] || 0;
+    }
+  }
+  return pattern;
+}
+
+function knuthMorrisPratt(text, string, t=0, s=0) {
+  var count = 0;
+  const pattern = buildPatternArray(string);
+  while (t < text.length){
+    if (text[t] === string[s]){
+      if (s === string.length - 1) count++;
+      t++;
+      s++;
+    } else if (s > 0) {
+      s = pattern[s-1] || 0;
+    } else t++;
+  }
+  return count;
 }
